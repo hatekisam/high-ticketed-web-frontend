250bde153c1181c72ebc21276bb69d678ac9aed8
Mugisha Yves <mugishayves189000@gmail.com>
2023-12-31 15:58:47 +0200

    made the document component

--- src/app/admin/documents/page.tsx
+++ src/app/admin/documents/page.tsx
@@ -1,9 +1,39 @@
-import React from 'react'
+"use client";
+import ADocument from "@/components/documents/ADocument";
+import React from "react";
 
 const Documents = () => {
+  const dummyData = [
+    {
+      format: "doc",
+      name: "Document 1",
+      size: 5,
+      uploadedAt: new Date("2023-01-15"),
+    },
+    {
+      format: "pdf",
+      name: "Document 2",
+      size: 8,
+      uploadedAt: new Date("2023-02-20"),
+    },
+  ];
   return (
-    <div>Documents</div>
-  )
-}
+    <div className="p-5 overflow-y-auto h-screen">
+      <p className="text-xl  mt-2 ">Employees</p>
+      <div className="mt-5 grid grid-cols-5 gap-4">
+        {dummyData.map((item, index) => (
+          <ADocument
+            key={index}
+            //@ts-ignore
+            format={item.format}
+            name={item.name}
+            size={item.size}
+            uploadedAt={item.uploadedAt}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default Documents
+export default Documents;
---
+++ src/components/documents/ADocument.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { DocIcon, PdfIcon } from "../core/icons";
+
+interface Props {
+  format: "pdf" | "doc";
+  name: string;
+  size: number;
+  uploadedAt: Date;
+}
+
+const ADocument: React.FC<Props> = ({ format, name, size, uploadedAt }) => {
+  return (
+    <div className="rounded-md bg-[#191A1D]">
+      <div className="flex items-center flex-col justify-center py-10">
+        {format === "doc" ? <DocIcon /> : <PdfIcon />}
+        <p>{name}</p>
+      </div>
+      <div className="border-t-2 border-t-gray-400 flex justify-between p-2 text-xs">
+        <p>
+          Size: <span>{size}mb</span>
+        </p>
+        <p>
+          Uploaded: <span>{new Date(uploadedAt).getDate()}</span>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default ADocument;
